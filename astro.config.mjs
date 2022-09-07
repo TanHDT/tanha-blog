5cc44eabe78182614613380337f805a38a41bb27
TanHa <hdtan.hcmus@gmail.com>
2022-09-07 23:36:40 +0700

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-template.netlify.app',
+  base: '/tanha-blog',
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
