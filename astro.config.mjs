6a61b216a3c60704c6dc5d5acf8dec0b52e29721
TanHa <hdtan.hcmus@gmail.com>
2022-09-13 00:27:05 +0700

    add tailwind and update UI blog page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,20 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-template.netlify.app',
-  base: '/tanha-blog',
-  integrations: [mdx(), svelte()],
+  base: '/',
+  integrations: [mdx(), svelte(), tailwind()],
   markdown: {
     shikiConfig: {
-      theme: 'nord',
+      theme: 'nord'
     },
-    rehypePlugins: [
-      [
-        'rehype-external-links',
-        {
-          target: '_blank',
-        },
-      ],
-    ],
-  },
-})
+    rehypePlugins: [['rehype-external-links', {
+      target: '_blank'
+    }]]
+  }
+});
--- src/data/blog-posts/hello-world.md
+++ src/data/blog-posts/hello-world.md
@@ -1,7 +1,11 @@
 ---
 title: Hello World 👋
 publishDate: 30 Nov 2021
+author: Pouya
 description: Every blog starts with a single post. This is yours. Make it great.
+img: https://api.lorem.space/image/furniture?w=500&h=500
+imgWidth: 500
+imgHeight: 500
 ---
 
 ![Illustration of woman using a meditation app](/assets/blog/casual-life-3d-meditation-crystal.webp)
